5bb57a2e3869bb0a35136236cad4287f388ee34d
alenvalek <valekalen@gmail.com>
2023-06-11 17:03:34 +0200

    initial commit

---
+++ src/main/java/dev/alenvalek/BeKind/BeKindApplication.java
@@ -0,0 +1,13 @@
+package dev.alenvalek.BeKind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeKindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeKindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alenvalek/BeKind/BeKindApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alenvalek.BeKind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeKindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
